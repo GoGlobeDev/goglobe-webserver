d142cf7d0e8da4c2c9e1388a201c042b3b9661d9
= <=>
2018-03-06 17:26:09 +0800

    goglobe

--- src/main/java/com/goglobe/controller/PingController.java
+++ src/main/java/com/goglobe/controller/PingController.java
@@ -1,5 +1,7 @@
 package com.goglobe.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/goglobe")
 public class PingController {
+	
+	private Logger logger = LogManager.getLogger(PingController.class);
+	
 	@RequestMapping(value = "/ping")
 	public String ping() {
 		return "goglobe running";
+	}
+	
+	@RequestMapping(value = "/log")
+	public String log() {
+		logger.error("error");
+		logger.info("info");
+		return "goglobe log";
 	}
 }
