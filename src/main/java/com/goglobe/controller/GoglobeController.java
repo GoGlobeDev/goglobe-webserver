984662edf7c2e18a9e24b0c18c953108d5b66528
= <=>
2018-03-06 11:55:14 +0800

    goglobe

--- src/main/java/com/goglobe/controller/GoglobeController.java
+++ src/main/java/com/goglobe/controller/GoglobeController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import com.goglobe.service.GoglobeService;
 
 @EnableAutoConfiguration
 @RestController
+@CrossOrigin
 @RequestMapping("/goglobe")
 public class GoglobeController {
 	@Autowired
